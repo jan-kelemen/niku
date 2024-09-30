13e67a19640e85f4f250f6317ee14b5d166ea641
Jan Kelemen <jkeleme@gmail.com>
2024-09-30 18:03:20 +0200

    Add helpers for descriptor infos

--- src/vkrndr/include/vkrndr_descriptors.hpp
+++ src/vkrndr/include/vkrndr_descriptors.hpp
@@ -1,6 +1,11 @@
 #ifndef VKRNDR_DESCRIPTORS_INCLUDED
 #define VKRNDR_DESCRIPTORS_INCLUDED
 
+#include <vkrndr_buffer.hpp>
+#include <vkrndr_image.hpp>
+
+#include <cppext_pragma_warning.hpp>
+
 #include <volk.h>
 
 #include <span>
@@ -20,6 +25,47 @@ namespace vkrndr
     void free_descriptor_sets(device_t const& device,
         VkDescriptorPool descriptor_pool,
         std::span<VkDescriptorSet> descriptor_sets);
+
+    [[nodiscard]] constexpr VkDescriptorBufferInfo buffer_descriptor(
+        vkrndr::buffer_t const& buffer,
+        VkDeviceSize const offset = 0,
+        VkDeviceSize const range = VK_WHOLE_SIZE)
+    {
+        return {buffer.buffer, offset, range};
+    }
+
+    [[nodiscard]] constexpr VkDescriptorImageInfo sampled_image_descriptor(
+        vkrndr::image_t const& image,
+        VkImageLayout const layout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL)
+    {
+        DISABLE_WARNING_PUSH
+        DISABLE_WARNING_MISSING_FIELD_INITIALIZERS
+
+        return {.imageView = image.view, .imageLayout = layout};
+
+        DISABLE_WARNING_POP
+    }
+
+    [[nodiscard]] constexpr VkDescriptorImageInfo sampler_descriptor(
+        VkSampler sampler)
+    {
+        DISABLE_WARNING_PUSH
+        DISABLE_WARNING_MISSING_FIELD_INITIALIZERS
+
+        return {.sampler = sampler};
+
+        DISABLE_WARNING_POP
+    }
+
+    [[nodiscard]] constexpr VkDescriptorImageInfo combined_sampler_descriptor(
+        VkSampler sampler,
+        vkrndr::image_t const& image,
+        VkImageLayout const layout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL)
+    {
+        return {.sampler = sampler,
+            .imageView = image.view,
+            .imageLayout = layout};
+    }
 } // namespace vkrndr
 
 #endif
