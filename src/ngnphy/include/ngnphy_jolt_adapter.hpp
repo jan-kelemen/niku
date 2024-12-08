c61af6bea89ab68ade91b045340b46df9eb5ba37
Jan Kelemen <jkeleme@gmail.com>
2024-12-08 15:20:04 +0100

    Resolve build error on MSVC

--- src/ngnphy/include/ngnphy_jolt_adapter.hpp
+++ src/ngnphy/include/ngnphy_jolt_adapter.hpp
@@ -1,41 +1,26 @@
 #ifndef NGNPHY_JOLT_ADAPTER_INCLUDED
 #define NGNPHY_JOLT_ADAPTER_INCLUDED
 
-#include <glm/gtc/type_ptr.hpp>
 #include <glm/mat4x4.hpp>
 #include <glm/vec3.hpp>
 #include <glm/vec4.hpp>
 
-#include <Jolt/Jolt.h> // IWYU pragma: keep
-#include <Jolt/Math/Mat44.h>
-#include <Jolt/Math/Vec3.h>
-#include <Jolt/Math/Vec4.h>
-
 // IWYU pragma: no_include <glm/detail/qualifier.hpp>
+
+namespace JPH
+{
+    class Vec3;
+    class Vec4;
+    class Mat44;
+} // namespace JPH
 
 namespace ngnphy
 {
-    [[nodiscard]] constexpr glm::vec3 to_glm(JPH::Vec3 const& v)
-    {
-        // NOLINTBEGIN(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
-        return glm::make_vec3(v.mF32);
-        // NOLINTEND(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
-    }
+    [[nodiscard]] glm::vec3 to_glm(JPH::Vec3 const& v);
 
-    [[nodiscard]] constexpr glm::vec4 to_glm(JPH::Vec4 const& v)
-    {
-        // NOLINTBEGIN(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
-        return glm::make_vec4(v.mF32);
-        // NOLINTEND(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
-    }
+    [[nodiscard]] glm::vec4 to_glm(JPH::Vec4 const& v);
 
-    [[nodiscard]] constexpr glm::mat4 to_glm(JPH::Mat44 const& v)
-    {
-        return {to_glm(v.GetColumn4(0)),
-            to_glm(v.GetColumn4(1)),
-            to_glm(v.GetColumn4(2)),
-            to_glm(v.GetColumn4(3))};
-    }
+    [[nodiscard]] glm::mat4 to_glm(JPH::Mat44 const& v);
 } // namespace ngnphy
 
 #endif
--- src/ngnphy/src/ngnphy_jolt_adapter.cpp
+++ src/ngnphy/src/ngnphy_jolt_adapter.cpp
@@ -1,1 +1,30 @@
 #include <ngnphy_jolt_adapter.hpp>
+
+#include <glm/gtc/type_ptr.hpp>
+
+#include <Jolt/Jolt.h> // IWYU pragma: keep
+#include <Jolt/Math/Mat44.h>
+#include <Jolt/Math/Vec3.h>
+#include <Jolt/Math/Vec4.h>
+
+glm::vec3 ngnphy::to_glm(JPH::Vec3 const& v)
+{
+    // NOLINTBEGIN(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
+    return glm::make_vec3(v.mF32);
+    // NOLINTEND(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
+}
+
+glm::vec4 ngnphy::to_glm(JPH::Vec4 const& v)
+{
+    // NOLINTBEGIN(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
+    return glm::make_vec4(v.mF32);
+    // NOLINTEND(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
+}
+
+glm::mat4 ngnphy::to_glm(JPH::Mat44 const& v)
+{
+    return {to_glm(v.GetColumn4(0)),
+        to_glm(v.GetColumn4(1)),
+        to_glm(v.GetColumn4(2)),
+        to_glm(v.GetColumn4(3))};
+}
