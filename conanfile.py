e014da147861e0f163375392992a04259f4423b1
Jan Kelemen <jkeleme@gmail.com>
2025-01-12 13:57:32 +0100

    Update Catch2, fmt, ImGui, SDL, simdjson and spdlog third party libraries

--- conanfile.py
+++ conanfile.py
@@ -15,18 +15,18 @@ class NikuConan(ConanFile):
         self.requires("boost/1.86.0")
         self.requires("entt/3.14.0")
         self.requires("fastgltf/0.8.0")
-        self.requires("fmt/11.0.2")
+        self.requires("fmt/11.1.1")
         self.requires("freetype/2.13.3")
         self.requires("glm/1.0.1")
         self.requires("glslang/1.3.296.0")
-        self.requires("imgui/1.91.5-docking")
+        self.requires("imgui/1.91.6-docking")
         self.requires("joltphysics/5.2.0")
         self.requires("mikktspace/cci.20200325")
         self.requires("meshoptimizer/0.22")
         self.requires("perlinnoise/3.0.0")
-        self.requires("sdl/2.30.9")
-        self.requires("simdjson/3.10.1")
-        self.requires("spdlog/1.15.0")
+        self.requires("sdl/2.30.11")
+        self.requires("simdjson/3.11.5")
+        self.requires("spdlog/1.15.x")
         self.requires("spirv-cross/1.3.296.0")
         self.requires("stb/cci.20240531")
         self.requires("volk/1.3.296.0")
@@ -34,7 +34,7 @@ class NikuConan(ConanFile):
 
     def build_requirements(self):
         self.tool_requires("cmake/[^3.27]")
-        self.test_requires("catch2/[^3.7.0]")
+        self.test_requires("catch2/3.8.0")
 
     def layout(self):
         cmake_layout(self)
