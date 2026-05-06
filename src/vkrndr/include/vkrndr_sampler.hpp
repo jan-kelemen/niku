9c802a2726a876380eb2bad0a672eb1fb0261565
Jan Kelemen <jkeleme@gmail.com>
2026-05-06 19:47:42 +0200

    std::isfinite isn't constexpr on MSVC, yeah...

--- src/vkrndr/include/vkrndr_sampler.hpp
+++ src/vkrndr/include/vkrndr_sampler.hpp
@@ -1,20 +1,13 @@
 #ifndef VKRNDR_SAMPLER_INCLUDED
 #define VKRNDR_SAMPLER_INCLUDED
 
-#include <vkrndr_utility.hpp>
-
 #include <cppext_hash.hpp> // IWYU pragma: keep
 
 #include <boost/hash2/hash_append_fwd.hpp> // IWYU pragma: keep
 
 #include <volk.h>
 
-#include <cassert>
 #include <optional>
-
-#ifndef NDEBUG
-#include <cmath>
-#endif
 
 // IWYU pragma: no_forward_declare boost::hash2::hash_append_tag
 
@@ -50,35 +43,8 @@ namespace vkrndr
         sampler_properties_t const& properties,
         bool enable_default_anisotropy);
 
-    [[nodiscard]] constexpr VkSamplerCreateInfo as_create_info(
-        sampler_properties_t const& properties)
-    {
-        assert(std::isfinite(properties.mip_lod_bias));
-        assert(std::isfinite(properties.min_lod));
-        assert(std::isfinite(properties.max_lod));
-        assert(!properties.max_anisotropy.has_value() ||
-            std::isfinite(*properties.max_anisotropy));
-
-        return {
-            .sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO,
-            .magFilter = properties.magnification_filter,
-            .minFilter = properties.minification_filter,
-            .mipmapMode = properties.mipmap_mode,
-            .addressModeU = properties.address_mode_u,
-            .addressModeV = properties.address_mode_v,
-            .addressModeW = properties.address_mode_w,
-            .mipLodBias = properties.mip_lod_bias,
-            .anisotropyEnable = to_bool(properties.max_anisotropy),
-            .maxAnisotropy = properties.max_anisotropy.value_or(1.0f),
-            .compareEnable = to_bool(properties.compare_op),
-            .compareOp = properties.compare_op.value_or(VK_COMPARE_OP_NEVER),
-            .minLod = properties.min_lod,
-            .maxLod = properties.max_lod,
-            .borderColor = properties.border_color,
-            .unnormalizedCoordinates =
-                to_bool(properties.unnormalized_coordinates),
-        };
-    }
+    [[nodiscard]] VkSamplerCreateInfo as_create_info(
+        sampler_properties_t const& properties);
 
     template<typename Provider, typename Hash, typename Flavor>
     constexpr void tag_invoke(boost::hash2::hash_append_tag const& /* tag */,
--- src/vkrndr/src/vkrndr_sampler.cpp
+++ src/vkrndr/src/vkrndr_sampler.cpp
@@ -1,8 +1,44 @@
 #include <vkrndr_sampler.hpp>
 
 #include <vkrndr_device.hpp>
+#include <vkrndr_utility.hpp>
 
 #include <algorithm>
+#include <cassert>
+#include <optional>
+
+#ifndef NDEBUG
+#include <cmath>
+#endif
+
+VkSamplerCreateInfo vkrndr::as_create_info(
+    sampler_properties_t const& properties)
+{
+    assert(std::isfinite(properties.mip_lod_bias));
+    assert(std::isfinite(properties.min_lod));
+    assert(std::isfinite(properties.max_lod));
+    assert(!properties.max_anisotropy.has_value() ||
+        std::isfinite(*properties.max_anisotropy));
+
+    return {
+        .sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO,
+        .magFilter = properties.magnification_filter,
+        .minFilter = properties.minification_filter,
+        .mipmapMode = properties.mipmap_mode,
+        .addressModeU = properties.address_mode_u,
+        .addressModeV = properties.address_mode_v,
+        .addressModeW = properties.address_mode_w,
+        .mipLodBias = properties.mip_lod_bias,
+        .anisotropyEnable = to_bool(properties.max_anisotropy),
+        .maxAnisotropy = properties.max_anisotropy.value_or(1.0f),
+        .compareEnable = to_bool(properties.compare_op),
+        .compareOp = properties.compare_op.value_or(VK_COMPARE_OP_NEVER),
+        .minLod = properties.min_lod,
+        .maxLod = properties.max_lod,
+        .borderColor = properties.border_color,
+        .unnormalizedCoordinates = to_bool(properties.unnormalized_coordinates),
+    };
+}
 
 VkSamplerCreateInfo vkrndr::as_create_info(vkrndr::device_t const& device,
     sampler_properties_t const& properties,
